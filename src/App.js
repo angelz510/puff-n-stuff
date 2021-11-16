441a426eaee1e9478a2a8a01661368b155b88e74
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2021-11-16 15:44:22 -0800

    Add(AgeAuth): Created new component to restrict under aged access

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import {useState} from 'react'
 import Nav from "./components/Nav";
+import AgeAuth from "./components/AgeAuth"
 import routes from "./config/routes";
 // import "./App.css";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false)
+
   return (
     <div className="App">
+      {!isAuth ? (<AgeAuth setIsAuth={setIsAuth} />
+      ) : (
+      <>
       <Nav />
       <div>{routes}</div>
+      </>
+      )}
     </div>
   );
 }
