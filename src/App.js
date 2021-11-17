ec3d32a167c3c5d0221422ce46da81511e2f7221
Angel R <angelzrod4@gmail.com>
2021-11-17 10:47:56 -0800

    styled home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Nav from "./components/Nav";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
 import routes from "./config/routes";
 import "./App.css";
 
@@ -7,7 +9,11 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <div className="content-container">{routes}</div>
+      <div className="content-container">
+        {/* {routes} */}
+        <Home />
+        <About />
+      </div>
     </div>
   );
 }
