c50d9b7e6c738b630fc09da2dbf4485f788dacd7
Angel R <angelzrod4@gmail.com>
2021-11-23 14:50:43 -0800

    styles the pages and added animations

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './Nav.css';
-import Logo from '../assets/puff-logo.jpeg';
+import React from "react";
+import "./Nav.css";
+import Logo from "../assets/puff-logo.jpeg";
 
 const MyNavbar = () => {
   return (
-    <div className='nav-container'>
-      <img src={Logo} className='nav-logo' alt='puff n stuff' />
-      <div className='nav-links'>
-        <div className='link'>Home</div>
-        <div className='link'>About</div>
-        <button className='link contact'>Contact</button>
+    <div className="nav-container">
+      <img src={Logo} className="nav-logo" alt="puff n stuff" />
+      <div className="nav-links">
+        <div className="link">Home</div>
+        <div className="link">About</div>
+        <button className="link contact">Contact</button>
       </div>
     </div>
   );
