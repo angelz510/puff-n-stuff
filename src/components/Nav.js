4a9048f8dbbc330512e5e6b69ef0b336cfe691ca
Angel R <angelzrod4@gmail.com>
2021-11-24 12:23:32 -0800

    cleaned code and applied media queries for responsiveness

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,16 +4,16 @@ import Logo from "../assets/puff-logo.jpeg";
 
 const MyNavbar = () => {
   return (
-    <div className='nav-container'>
-      <img src={Logo} className='nav-logo' alt='puff n stuff' />
-      <div className='nav-links'>
-        <a href='#Home' className='link'>
+    <div className="nav-container">
+      <img src={Logo} className="nav-logo" alt="puff n stuff" />
+      <div className="nav-links">
+        <a href="#Home" className="link">
           Home
         </a>
-        <a href='#About' className='link'>
+        <a href="#About" className="link">
           About
         </a>
-        <a href='#Contact' className='link'>
+        <a href="#Contact" className="link">
           Contact
         </a>
       </div>
